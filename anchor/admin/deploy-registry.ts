b4d5429419ecc5b619bdfb260e0d7008f82dd36d
calc1r4r <20yashs09@gmail.com>
2024-10-05 21:38:24 +0530

    Changed to same deployer key

--- anchor/admin/deploy-registry.ts
+++ anchor/admin/deploy-registry.ts
@@ -1,8 +1,9 @@
 import * as fs from 'fs';
 import * as bs58 from 'bs58';
 import * as dotenv from 'dotenv';
+import * as anchor from '@coral-xyz/anchor';
 
-import { Program, workspace, setProvider, BN } from '@coral-xyz/anchor';
+import { Program, workspace, setProvider } from '@coral-xyz/anchor';
 
 import {
   Keypair,
@@ -16,49 +17,26 @@ import {
 import { TOKEN_PROGRAM_ID, createMint } from '@solana/spl-token';
 
 import { Gigentic } from '../target/types/gigentic';
-
 import { PROVIDER, SERVICE_REGISTRY_KEYPAIR } from '../tests/constants';
 
 dotenv.config();
 
-// Start of Selection
-function loadSecrekeyFromEnv(envVarName: string): Keypair {
-  console.log(`Loading secret key from environment variable: ${envVarName}`);
+// Load secret key from environment variable
+function loadKeypairFromEnv(envVarName: string): Keypair {
   const keypairPath = process.env[envVarName];
-  if (!keypairPath) {
-    throw new Error(`${envVarName} is not set in environment variables`);
-  }
-  console.log(`Keypair path found: ${keypairPath}`);
+  if (!keypairPath) throw new Error(`${envVarName} is not set`);
   const keypairData = JSON.parse(fs.readFileSync(keypairPath, 'utf8'));
-  console.log(`Keypair data loaded from ${keypairPath}`);
-  const keypair = Keypair.fromSecretKey(new Uint8Array(keypairData));
-  console.log(
-    `Keypair generated with public key: ${keypair.publicKey.toString()}`,
-  );
-  console.log('Public key of the keypair generated : ', keypair.publicKey);
-  return keypair;
+  return Keypair.fromSecretKey(new Uint8Array(keypairData));
 }
 
+// Load bs58-encoded keypair from environment variable
 export function loadKeypairBs58FromEnv(envVarName: string): Keypair {
-  console.log(
-    `Loading keypair from bs58-encoded environment variable: ${envVarName}`,
-  );
   const encodedKey = process.env[envVarName];
-  if (!encodedKey) {
-    throw new Error(`${envVarName} is not set in environment variables`);
-  }
-  console.log(`Decoding bs58 keypair: ${encodedKey}`);
-  const decodedKey = bs58.decode(encodedKey);
-
-  // Create keypair directly from the decoded bytes
-  const keypair = Keypair.fromSecretKey(decodedKey);
-
-  console.log(
-    `Keypair generated with public key: ${keypair.publicKey.toString()}`,
-  );
-  return keypair;
+  if (!encodedKey) throw new Error(`${envVarName} is not set`);
+  return Keypair.fromSecretKey(bs58.decode(encodedKey));
 }
-// Configure the client to use the local cluster.
+
+// Configure the client to use the local cluster
 setProvider(PROVIDER);
 
 // Initialize connection to the Solana network using the provided anchor provider
@@ -69,20 +47,26 @@ export const program: Program<Gigentic> =
   workspace.Gigentic as Program<Gigentic>;
 
 // Load the "programDeployer" admin keypair which is used to deploy the program and create the service registry
-// const programDeployerKeypair = JSON.parse(
-//   fs.readFileSync(DEPLOYER_KEYPAIR_PATH, 'utf8'),
-// );
-
 const programDeployer = loadKeypairBs58FromEnv('PROGRAM_DEPLOYER');
 console.log('programDeployer', programDeployer.publicKey.toString());
-
-// const programDeployer = Keypair.fromSecretKey(new Uint8Array(programDeployerKeypair));
-// console.log('programDeployer', programDeployer.publicKey.toString());
 
 const serviceDeployer = loadKeypairBs58FromEnv('SERVICE_DEPLOYER');
 console.log('serviceDeployer', serviceDeployer.publicKey.toString());
 
 let mint: PublicKey;
+
+async function airdrop(deployer: PublicKey) {
+  try {
+    const airdropSignature = await connection.requestAirdrop(
+      deployer,
+      10000 * anchor.web3.LAMPORTS_PER_SOL, // Airdrop 2 SOL
+    );
+    await connection.confirmTransaction(airdropSignature, 'confirmed');
+    console.log(`Airdropped 2 SOL to ${deployer.toString()}`);
+  } catch (error) {
+    console.error('Error airdropping SOL:', error);
+  }
+}
 
 async function initServiceRegistry() {
   try {
@@ -102,7 +86,7 @@ async function initServiceRegistry() {
     console.log('Fee Percentage:', feePercentage);
 
     // Create the service registry account
-    const serviceRegistryAccountSize = 2000000; // Adjust the size based on the ServiceRegistry struct
+    const serviceRegistryAccountSize = 2000; // Adjust the size based on the ServiceRegistry struct
     const rentExemptionAmount =
       await connection.getMinimumBalanceForRentExemption(
         serviceRegistryAccountSize,
@@ -162,8 +146,8 @@ async function initServiceRegistry() {
 
 async function main() {
   try {
+    await Promise.all([airdrop(programDeployer.publicKey), airdrop(serviceDeployer.publicKey)]);
     await initServiceRegistry();
-    // await createService();
   } catch (error) {
     console.error('Error in main execution:', error);
   }
