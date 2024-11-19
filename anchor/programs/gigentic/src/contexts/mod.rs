b47f7f7e9d355dc5b0a6c9051ac912a3b7b2588c
Marton Csernai <34937334+csmarc@users.noreply.github.com>
2024-11-19 21:26:36 +0100

    refactor "agent" to "provider"

--- anchor/programs/gigentic/src/contexts/mod.rs
+++ anchor/programs/gigentic/src/contexts/mod.rs
@@ -1,6 +1,6 @@
-pub mod agent_to_consumer_rating;
-pub mod consumer_to_agent_rating;
+pub mod consumer_to_provider_rating;
 pub mod init_service;
 pub mod init_service_registry;
 pub mod pay_service;
+pub mod provider_to_consumer_rating;
 pub mod sign_service;
