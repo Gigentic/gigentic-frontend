b543ac97e21f0dc0b3729820cc2d2f31cf207e28
Marton Csernai <34937334+csmarc@users.noreply.github.com>
2024-11-25 18:14:14 +0100

    list only services of the service registry

--- web/lib/hooks/blockchain/use-gigentic-program.ts
+++ web/lib/hooks/blockchain/use-gigentic-program.ts
@@ -1,20 +1,16 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 'use client';
 
 import {
   getGigenticProgram,
   getGigenticProgramId,
 } from '@gigentic-frontend/anchor';
-import { Connection, Cluster, PublicKey } from '@solana/web3.js';
+import { Cluster } from '@solana/web3.js';
 import { useConnection } from '@solana/wallet-adapter-react';
 import { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useAnchorProvider } from '@/providers/solana-provider';
 import { useCluster } from '@/cluster/cluster-data-access';
-
-export async function getBlockhash(connection: Connection, pubkey: PublicKey) {
-  const latestBlockhash = await connection.getLatestBlockhash();
-  console.log('latestBlockhash', latestBlockhash);
-}
 
 export function useGigenticProgram() {
   const { connection } = useConnection();
@@ -26,13 +22,37 @@ export function useGigenticProgram() {
   );
   const program = getGigenticProgram(provider);
 
-  // can be removed/refactored when cleaning up gigentic-frontend-feature
-  const accounts = useQuery({
-    queryKey: ['gigentic', 'all', { cluster }],
-    queryFn: () => program.account.service.all(),
+  const serviceRegistry = useQuery({
+    queryKey: ['service-registry', { cluster }],
+    queryFn: async () => {
+      const registry = await program.account.serviceRegistry.fetch(
+        process.env.NEXT_PUBLIC_SERVICE_REGISTRY_PUBKEY!,
+      );
+      return registry;
+    },
   });
 
-  // can be removed/refactored when cleaning up gigentic-frontend-feature
+  const accounts = useQuery({
+    queryKey: ['services', { cluster }],
+    queryFn: async () => {
+      if (!serviceRegistry.data) return [];
+
+      // Fetch all services from addresses in registry
+      const services = await Promise.all(
+        serviceRegistry.data.serviceAccountAddresses.map(async (address) => {
+          const account = await program.account.service.fetch(address);
+          return {
+            publicKey: address,
+            account,
+          };
+        }),
+      );
+
+      return services;
+    },
+    enabled: !!serviceRegistry.data,
+  });
+
   const getProgramAccount = useQuery({
     queryKey: ['get-program-account', { cluster }],
     queryFn: () => connection.getParsedAccountInfo(programId),
