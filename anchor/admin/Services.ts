38f26af13dd037380eae5b61283a2c2476cf7cd7
Marton Csernai <34937334+csmarc@users.noreply.github.com>
2024-11-25 07:35:58 +0100

    delete chatwalletaddress and avgrating from Services file

--- anchor/admin/Services.ts
+++ anchor/admin/Services.ts
@@ -5,16 +5,12 @@ export const services = [
     title: 'Backend Developer',
     experience:
       'Specialist in Node.js and NoSQL databases, 3 years working in fast paced startup environment',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 2.9,
   },
   {
     price: 0.02,
     currency: 'SOL',
     title: 'Frontend Developer',
     experience: 'Specialist in React, also some experience in UX design',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 4.2,
   },
   {
     price: 0.025,
@@ -22,8 +18,6 @@ export const services = [
     title: 'Senior Full Stack Developer',
     experience:
       'Specialist in Next.js in combination with Postgres databases, 5 years of experience in the field',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 4.1,
   },
   {
     price: 0.02,
@@ -31,8 +25,6 @@ export const services = [
     title: 'DevOps Engineer',
     experience:
       'Specialist in Docker and Kubernetes, 2 years of experience migrating and managing large scale applications',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.8,
   },
   {
     price: 0.025,
@@ -40,8 +32,6 @@ export const services = [
     title: 'Data Scientist',
     experience:
       'Specialist in Python, 4 years of experience in the field, PhD in Mathematics, Skills: PyTorch, TensorFlow, Scikit-learn',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.9,
   },
   {
     price: 0.02,
@@ -58,8 +48,6 @@ export const services = [
     title: 'Smart Contract Auditor',
     experience:
       '5 years of experience in auditing smart contracts, Chains: ADA, DOT',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 4.8,
   },
   {
     price: 0.03,
@@ -67,24 +55,18 @@ export const services = [
     title: 'Smart Contract Audit AI Agent',
     experience:
       '120 successful projects with excellent customer feedback, Chains: SOL, ETH',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 4.9,
   },
   {
     price: 0.03,
     currency: 'SOL',
     title: 'Smart Contract Developer',
     experience: 'Specialist in Solidity, 4 years of experience in the field',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.2,
   },
   {
     price: 0.025,
     currency: 'SOL',
     title: 'Blockchain Developer',
     experience: 'Specialist in Rust, 2 years of experience in the field',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.1,
   },
   {
     price: 0.02,
@@ -92,8 +74,6 @@ export const services = [
     title: 'Solana Developer',
     experience:
       '3 years of experience in Solana. Worked with Rust on various L2 projects. Strong experience with Anchor and TypeScript. Participated in various Hackthons (Radar, WebZero, Colosseum). Located in Dublin and open to work remotely in any timezone',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.9,
   },
   {
     price: 0.015,
@@ -101,8 +81,6 @@ export const services = [
     title: 'Rust Developer',
     experience:
       '8 years of experience in Rust, experience in coaching and mentoring junior developers',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.5,
   },
   {
     price: 0.01,
@@ -110,31 +88,23 @@ export const services = [
     title: 'Python Developer',
     experience:
       '4 years of experience in Python, 2 years of experience in AI/ML',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.3,
   },
   {
     price: 0.012,
     currency: 'SOL',
     title: 'Javascript Developer',
     experience: '3 years of experience in Javascript',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.7,
   },
   {
     price: 0.015,
     currency: 'SOL',
     title: 'React Developer',
     experience: '2 years of experience in React',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 4.1,
   },
   {
     price: 0.01,
     currency: 'SOL',
     title: 'Next.js Developer',
     experience: '2 years of experience in Next.js, various css frameworks',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 4.0,
   },
 ];
