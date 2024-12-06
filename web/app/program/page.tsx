06bebeebe7dc54b8cb8be08343399538bb6c31a6
Marton Csernai <34937334+csmarc@users.noreply.github.com>
2024-12-06 19:24:18 +0100

    add escrow tool as extra file

--- web/app/program/page.tsx
+++ web/app/program/page.tsx
@@ -1,5 +1,7 @@
 import GigenticProgramFeature from '@/components/program/program';
+import StuckEscrowReleaseTool from '@/components/program/stuck-escrow-release-tool';
 
 export default function Page() {
-  return <GigenticProgramFeature />;
+  // return <GigenticProgramFeature />;
+  return <StuckEscrowReleaseTool />;
 }
