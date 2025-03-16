1628503d822bedccb71571ca3730a1490f6d26d8
Marton Csernai <34937334+csmarc@users.noreply.github.com>
2025-03-16 22:45:32 +0100

    deploy to sonic

--- anchor/admin/Services.ts
+++ anchor/admin/Services.ts
@@ -33,78 +33,78 @@ export const services = [
     description:
       'Specialist in Python, 4 years of experience in the field, PhD in Mathematics, Skills: PyTorch, TensorFlow, Scikit-learn',
   },
-  {
-    price: 0.02,
-    currency: 'SOL',
-    title: 'Machine Learning Engineer',
-    description:
-      'Specialist in TensorFlow, 3 years of experience in the field, specialized in ML Ops',
-    chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
-    avgRating: 3.7,
-  },
-  {
-    price: 0.025,
-    currency: 'SOL',
-    title: 'Smart Contract Auditor',
-    description:
-      '5 years of experience in auditing smart contracts, Chains: ADA, DOT',
-  },
-  {
-    price: 0.03,
-    currency: 'SOL',
-    title: 'Smart Contract Audit AI Agent',
-    description:
-      '120 successful projects with excellent customer feedback, Chains: SOL, ETH',
-  },
-  {
-    price: 0.03,
-    currency: 'SOL',
-    title: 'Smart Contract Developer',
-    description: 'Specialist in Solidity, 4 years of experience in the field',
-  },
-  {
-    price: 0.025,
-    currency: 'SOL',
-    title: 'Blockchain Developer',
-    description: 'Specialist in Rust, 2 years of experience in the field',
-  },
-  {
-    price: 0.02,
-    currency: 'SOL',
-    title: 'Solana Developer',
-    description:
-      '3 years of experience in Solana. Worked with Rust on various L2 projects. Strong experience with Anchor and TypeScript. Participated in various Hackthons (Radar, WebZero, Colosseum). Located in Dublin and open to work remotely in any timezone',
-  },
-  {
-    price: 0.015,
-    currency: 'SOL',
-    title: 'Rust Developer',
-    description:
-      '8 years of experience in Rust, experience in coaching and mentoring junior developers',
-  },
-  {
-    price: 0.01,
-    currency: 'SOL',
-    title: 'Python Developer',
-    description:
-      '4 years of experience in Python, 2 years of experience in AI/ML',
-  },
-  {
-    price: 0.012,
-    currency: 'SOL',
-    title: 'Javascript Developer',
-    description: '3 years of experience in Javascript',
-  },
-  {
-    price: 0.015,
-    currency: 'SOL',
-    title: 'React Developer',
-    description: '2 years of experience in React',
-  },
-  {
-    price: 0.01,
-    currency: 'SOL',
-    title: 'Next.js Developer',
-    description: '2 years of experience in Next.js, various css frameworks',
-  },
+  // {
+  //   price: 0.02,
+  //   currency: 'SOL',
+  //   title: 'Machine Learning Engineer',
+  //   description:
+  //     'Specialist in TensorFlow, 3 years of experience in the field, specialized in ML Ops',
+  //   chatWalletAddress: 'GxZwMR34EYkWm49qfPkvRG5zGJzJg1mpbFCPkPbD3Eem',
+  //   avgRating: 3.7,
+  // },
+  // {
+  //   price: 0.025,
+  //   currency: 'SOL',
+  //   title: 'Smart Contract Auditor',
+  //   description:
+  //     '5 years of experience in auditing smart contracts, Chains: ADA, DOT',
+  // },
+  // {
+  //   price: 0.03,
+  //   currency: 'SOL',
+  //   title: 'Smart Contract Audit AI Agent',
+  //   description:
+  //     '120 successful projects with excellent customer feedback, Chains: SOL, ETH',
+  // },
+  // {
+  //   price: 0.03,
+  //   currency: 'SOL',
+  //   title: 'Smart Contract Developer',
+  //   description: 'Specialist in Solidity, 4 years of experience in the field',
+  // },
+  // {
+  //   price: 0.025,
+  //   currency: 'SOL',
+  //   title: 'Blockchain Developer',
+  //   description: 'Specialist in Rust, 2 years of experience in the field',
+  // },
+  // {
+  //   price: 0.02,
+  //   currency: 'SOL',
+  //   title: 'Solana Developer',
+  //   description:
+  //     '3 years of experience in Solana. Worked with Rust on various L2 projects. Strong experience with Anchor and TypeScript. Participated in various Hackthons (Radar, WebZero, Colosseum). Located in Dublin and open to work remotely in any timezone',
+  // },
+  // {
+  //   price: 0.015,
+  //   currency: 'SOL',
+  //   title: 'Rust Developer',
+  //   description:
+  //     '8 years of experience in Rust, experience in coaching and mentoring junior developers',
+  // },
+  // {
+  //   price: 0.01,
+  //   currency: 'SOL',
+  //   title: 'Python Developer',
+  //   description:
+  //     '4 years of experience in Python, 2 years of experience in AI/ML',
+  // },
+  // {
+  //   price: 0.012,
+  //   currency: 'SOL',
+  //   title: 'Javascript Developer',
+  //   description: '3 years of experience in Javascript',
+  // },
+  // {
+  //   price: 0.015,
+  //   currency: 'SOL',
+  //   title: 'React Developer',
+  //   description: '2 years of experience in React',
+  // },
+  // {
+  //   price: 0.01,
+  //   currency: 'SOL',
+  //   title: 'Next.js Developer',
+  //   description: '2 years of experience in Next.js, various css frameworks',
+  // },
 ];
